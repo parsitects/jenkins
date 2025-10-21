b762d1d91ab5528e3bc43af52ccfed90d3c07c85
jjrush <jjrush-github@proton.me>
2025-10-21 16:09:41 -0600

    Testing cron triggers

--- vars/build.groovy
+++ vars/build.groovy
@@ -35,10 +35,13 @@ def call(Map config = [:]) {
     // Parsitects on Saturday, CISA on Sunday
     def cronSchedule
     if (env.JOB_NAME.contains('parsitects')) {
-        cronSchedule = config.cronSchedule ?: 'H H(8-17) * * 6'  // Spread Parsitects builds between 8am and 5pm on Saturday
-    } else {
-        cronSchedule = config.cronSchedule ?: 'H H(8-17) * * 7'  // Spread CISA builds between 8am and 5pm on Sunday
+        cronSchedule = config.cronSchedule ?: '*/10 * * * *'  // Every 10 minutes - TEST ONLY
     }
+    // if (env.JOB_NAME.contains('parsitects')) {
+    //     cronSchedule = config.cronSchedule ?: 'H H(8-17) * * 6'  // Spread Parsitects builds between 8am and 5pm on Saturday
+    // } else {
+    //     cronSchedule = config.cronSchedule ?: 'H H(8-17) * * 7'  // Spread CISA builds between 8am and 5pm on Sunday
+    // }
 
     pipeline {
         agent {
